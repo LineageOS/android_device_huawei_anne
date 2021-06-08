df817c1aabb9d6913531eda6384fc0a5bce6ec3f
Haky86 <hakamelassouad@gmail.com>
2021-06-09 00:55:23 +0200

    anne: Inherit aosp_arm64_ab
    * Needed for legacy devices such anne and figo.
    
    Test: Build and boot into Home.
    
    Signed-off-by: Haky86 <hakamelassouad@gmail.com>
    Change-Id: I44e71c29807507ebbccca70f8aa69aeea11daa5a

--- lineage_anne.mk
+++ lineage_anne.mk
@@ -15,6 +15,7 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_arm64_ab.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
